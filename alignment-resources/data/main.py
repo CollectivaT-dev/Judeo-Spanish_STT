00eec5d97ff292ae2f4ecb1912f83f865eb8ec2d
Rodolfo Zevallos <20051487+rjzevallos@users.noreply.github.com>
2022-05-17 20:07:05 +0200

    update data/main.py

--- alignment-resources/data/main.py
+++ alignment-resources/data/main.py
@@ -2,25 +2,18 @@ import os
 import util
 import argparse
 
+FILEPATH = os.path.dirname('/mnt/d/Collectivat/ladino/Ladino_STT/')
+RAW_PATH = os.path.join(FILEPATH, 'alignment-resources/raw')
+PROC_PATH = os.path.join(FILEPATH, 'alignment-resources/process')
+
 
 def main():
-    parser = argparse.ArgumentParser("Parse Text")
-    parser.add_argument("-f", "--file", help="file to be normalized.")
-    args = parser.parse_args()
     
-    path_file = args.file
-    path_write = path_file[:-4]+"_norm.txt"
-    with open(path_write, 'w') as file_w:
-        with open(path_file) as file:
-            lines = file.readlines()
-            for line in lines:
-                if line.strip() != "":
-                    line = util.normalizer(line)
-                    numbers = [int(s.replace(".","").replace(",","")) for s in line.split() if s.replace(".","").replace(",","").isdigit()]
-                    for number in numbers:
-                        line = line.replace(str(number),(util.num_let(number)))
-                    file_w.write(line+"\n")
-
-
+    parser = argparse.ArgumentParser("Parse Text and WAV")
+    parser.add_argument("-p", "--path", help="Project's path.")
+    args = parser.parse_args()
+    FILEPATH = args.path
+    util.text_wav_normalized(FILEPATH)
+    
 if __name__ == '__main__':
     main()
--- alignment-resources/data/util.py
+++ alignment-resources/data/util.py
@@ -1,3 +1,11 @@
+
+import subprocess
+import os
+import re
+
+FILEPATH = os.path.dirname('')
+RAW_PATH = os.path.join(FILEPATH, 'alignment-resources/raw')
+PROC_PATH = os.path.join(FILEPATH, 'alignment-resources/process')
 MAX_NUMERO = 999999999999
 
 UNIDADES = (
@@ -144,6 +152,9 @@ def leer_millardos(numero):
     
     
 def normalizer(text):
+    text = re.sub('\ufeff| {2,}|\t', ' ', text)
+    text = re.sub('\n{2,}', '\n', text)
+    text = re.sub('\w\w\d\d\d.+', '', text)
     text = text.replace("...",".")
     text = text.replace("- ","")
     text = text.replace("…",".")
@@ -155,5 +166,88 @@ def normalizer(text):
     text = text.replace("\n","")
     text = text.replace("_","")
     text = text.replace("..","")
+    if text.find("BL0") != -1:
+        text = text[:text.find("BV0")]
     text = " ".join(text.split())
     return text
+    
+    
+def convert_doc(doc_file, out_dir, filename):
+    args = ['soffice', '--headless', '--convert-to', 'txt', doc_file,'--outdir', out_dir]
+    subprocess.call(args)
+    # Assumes there only one txt file exists
+    txt = [f for f in os.listdir(out_dir) if f.endswith('txt')][0]
+    conv_file_path = os.path.join(out_dir, txt)
+    with open(conv_file_path) as conv_file, open(os.path.join(out_dir, filename), 'w') as final_file:
+        lines = conv_file.readlines()
+        for line in lines:
+            if line.strip() != "":
+                line = normalizer(line)
+                numbers = [int(s.replace(".","").replace(",","")) for s in line.split() if s.replace(".","").replace(",","").isdigit()]
+                for number in numbers:
+                    line = line.replace(str(number),(util.num_let(number)))
+                final_file.write(line+"\n")
+    os.remove(conv_file_path)
+
+
+def get_processed_dirs(top_path, query):
+    paths = os.listdir(top_path)
+    dirs = []
+    for path in paths:
+        if os.path.isfile(os.path.join(top_path, path,'wav', path+query)):
+            dirs.append(path)
+    return dirs
+    
+
+def process(in_dir, out_dir):
+    '''Create MFA compatible dir, convert the doc files and the wav files
+    '''
+    rel_in_dir = os.path.join(RAW_PATH, in_dir)
+    rel_out_dir = os.path.join(PROC_PATH, out_dir)
+    if not os.path.isdir(rel_out_dir):
+        os.makedirs(rel_out_dir)
+    doc_file, wav_file = get_doc_wav(rel_in_dir)
+    convert_doc(doc_file, rel_out_dir, out_dir+'.txt')
+    convert_wav(wav_file, rel_out_dir, out_dir+'.wav')
+    
+    
+def convert(string):
+    return string.replace(' ','_').lower()
+
+
+def get_doc_wav(path):
+    for f in os.listdir(path):
+        f_low = f.lower()
+        f_path = os.path.join(path, f)
+        if f_low.endswith('doc') or f_low.endswith('docx'):
+            doc = f_path
+        elif f_low.endswith('wav') or f_low.endswith('mp3'):
+            wav = f_path
+    return doc, wav
+    
+
+def convert_wav(wav_file, out_dir, filename):
+    out_filepath = os.path.join(out_dir, filename)
+    args = ['ffmpeg', '-y', '-hide_banner', '-loglevel', 'panic',\
+            '-i', wav_file, '-ac', '1', '-ar', '16000', out_filepath]
+    subprocess.call(args)
+    
+    
+def text_wav_normalized(PATH):
+    FILEPATH = os.path.dirname(PATH)
+    raw_dirs = set([convert(d) for d in os.listdir(RAW_PATH)])
+    raw_dirs_dict = {convert(d):d for d in os.listdir(RAW_PATH)}
+    proc_dirs = get_processed_dirs(PROC_PATH, '.wav')
+    missing_dirs = list(raw_dirs.difference(proc_dirs))
+    print(missing_dirs)
+    for directory in missing_dirs:
+        print(directory)
+        process(raw_dirs_dict[directory], directory)
+
+    norm_dirs = get_processed_dirs(PROC_PATH, '_norm.txt')
+    missing_norm_dirs = list(raw_dirs.difference(norm_dirs))
+    for directory in missing_norm_dirs:
+        print('normalizing text of %s'%(directory))
+        text_source = os.path.join(PROC_PATH, directory, 'wav',
+                                   directory+'.txt')
+        text_target = text_source.replace('.txt', '_norm.txt')
